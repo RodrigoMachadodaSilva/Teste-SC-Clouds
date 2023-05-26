832edd6ba11492814dd1c13df01ac3cd770d4057
RodrigoMachadodaSilva <87843482+RodrigoMachadodaSilva@users.noreply.github.com>
2023-05-26 08:53:58 -0300

    Organização do código

--- src/teste/Funcoes.java
+++ src/teste/Funcoes.java
@@ -81,27 +81,27 @@ public class Funcoes {
 	}
 
 	public static Integer retornaPrimosMenoresRecursivo(Integer n) throws Exception {
-		 if(n>1) {
+		if (n > 1) {
 
-			n = n -1;
+			n = n - 1;
 			boolean primo = true;
-	
+
 			if (n == 1)
 				primo = false;
-			
+
 			if (primo) {
 				if ((n / (n - 1) == 0)) {
 					primo = false;
-	
+
 				}
 			}
-	
+
 			if (primo) {
 				if ((n % 2 == 0) && (n != 2)) {
 					primo = false;
 				}
 			}
-	
+
 			if (primo)
 				System.out.print(n);
 			if (n > 2) {
@@ -109,38 +109,8 @@ public class Funcoes {
 			}
 			return n;
 
-	}
-	 throw new Exception("Digite um número inteiro maior que 1");
-	}
-	public static boolean isPrime(int n, int i) {
-
-		// Base cases
-		if (n <= 2)
-			return (n == 2) ? true : false;
-		if (n % i == 0)
-			return false;
-		if (i * i > n)
-			return true;
-
-		// Check for next divisor
-		return isPrime(n, i + 1);
-	}
-
-	public static Integer testeDivisor(Integer a, Integer b) {
-		if (b == 1) {
-			return 0;
-		} else if (a % b == 0) {
-			return 1;
 		}
-		return testeDivisor(a, b - 1);
-
+		throw new Exception("Digite um número inteiro maior que 1");
 	}
 
-	public static Integer testaPrimo(Integer n) {
-		if (testeDivisor(n, n - 1) == 0) {
-			return 1;
-		}
-		return 0;
-
-	}
 }
